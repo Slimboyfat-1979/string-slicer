683513e9bcbb226d89c5d99ca95eb5d267668e6c
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2026-02-28 17:45:04 +1300

    Enhance string slicer functionality and UI
    - Updated input fields to default to 0 for better user experience
    - Added metadata display for sliced string details
    - Improved styling for better layout and readability

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const mainInput = document.querySelector(".input p");
 const input = document.querySelector(".input input");
 const sliceValues = document.querySelectorAll(".form-control input");
-const mainOutput = document.querySelector(".sliced-output")
+const mainOutput = document.querySelector(".sliced-output");
 
 const sliceIndex = [];
 
+window.addEventListener('DOMContentLoaded', () => {
+    sliceIndex[0] = 0;
+    sliceIndex[1] = 0;
+})
+
 input.addEventListener("keyup", (e) => {
   mainInput.textContent = e.target.value;
+  mainOutput.textContent = e.target.value;
 });
 
 sliceValues.forEach((value) => {
   value.addEventListener("input", (e) => {
+    if(e.target.value <= 0) {
+        e.target.value = 0;
+    }
     const type = e.target.dataset.type;
     const value = e.target.value;
     if (type === "firstIndex") {
@@ -30,7 +39,37 @@ function runSlicer(sliceIndex) {
   let [first, second] = sliceIndex;
   first = Number(first);
   second = Number(second);
-  const sliced = mainInput.textContent.slice(first, second);
-  console.log(typeof sliced);
-  mainOutput.textContent = sliced;
+  console.log(input.value);
+
+  const letters = mainInput.textContent.split("");
+  console.log(letters);
+  const sliced = letters.map((letter, idx) => {
+    if (idx >= first && idx < second) {
+      return `<span style="color: yellow; background-color:black;">${letter}</span>`;
+    }else{
+        return letter;
+    }
+  });
+  mainInput.textContent = mainInput.textContent.toLowerCase();
+  const metaData = {
+    string: mainInput.textContent,
+    length: mainInput.textContent.length,
+    startIndex: first,
+    endIndex: second,
+    firstIndexletter: mainInput.textContent[sliceIndex[0]],
+    secondIndexLetter: mainInput.textContent[sliceIndex[1]]
+  }
+
+  renderMetadata(metaData)
+
+  mainOutput.innerHTML = sliced.join("");
+}
+
+function renderMetadata(data) {
+    const fieldSet = document.getElementById("meta");
+    const pre = document.createElement("pre")
+    const metaObject =  JSON.stringify(data, null, 2)
+    pre.textContent = metaObject;
+    fieldSet.appendChild(pre)
+    fieldSet.scrollTop = fieldSet.scrollHeight
 }
